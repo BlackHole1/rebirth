8a529759d526a947d9aa2793112b5f1d9cb10b7b
Black-Hole <158blackhole@gmail.com>
2019-05-17 10:32:52 +0800

    refactor(log): optimize log output
    1. fix delete data fail in mysql

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,7 +1,9 @@
 const { type } = require('os');
 const { CHROME_PATH_LINUX, CHROME_PATH_MAC, USER_DATA_DIR_MAC, USER_DATA_DIR_LINUX } = require('./constants');
 
-module.exports.ToString = val => typeof val === 'string' ? val : JSON.stringify(val);
+const ToString = val => typeof val === 'string' ? val : JSON.stringify(val);
+
+module.exports.ToString = ToString;
 
 module.exports.paramsToObj = params => {
   if (!params) return {};
@@ -18,6 +20,16 @@ module.exports.paramsToObj = params => {
   return result;
 };
 
+module.exports.log = (desc, info) => {
+  console.log(`
+/**
+ * ${desc}
+ *
+ * ${ToString(info)}
+ *
+ */`);
+};
+// 优化日志输出
 const isMac = type() === 'Darwin';
 
 module.exports.chromePath = isMac ? CHROME_PATH_MAC : CHROME_PATH_LINUX;
