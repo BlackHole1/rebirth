15aff698883b095151ab02dc74555e1652f13fe1
BlackHole1 <158blackhole@gmail.com>
2019-07-21 15:09:51 +0800

    fix(db): fix SQL function name error

--- src/controllers/recordFail.js
+++ src/controllers/recordFail.js
@@ -3,14 +3,14 @@ const recordTasks = require('../lib/recordTasks');
 const servicesStatus = require('../lib/servicesStatus');
 const weblog = require('../lib/weblog');
 const utils = require('../lib/utils');
-const { setTaskStatusIsRecordFail } = require('../lib/SQLConstants');
+const { setTaskStatusIsFail } = require('../lib/SQLConstants');
 
 // 录制失败
 const recordFail = (req, res) => {
   const { id } = req.query;
   mysqlService.getConnection()
     .then(async conn => {
-      const result = await utils.SQLHandle(conn, setTaskStatusIsRecordFail, 'setTaskStatusIsRecordFail')(id);
+      const result = await utils.SQLHandle(conn, setTaskStatusIsFail, 'setTaskStatusIsFail')(id);
       conn.release();
       recordTasks.completeTask = id;
 
